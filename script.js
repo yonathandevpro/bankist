f9943fc98d5a596ae026feb1d8b67bf9ffd101a0
yonathandevpro <yonathanber@icloud.com>
2024-08-13 16:48:55 +0300

    implemented delete functionality

--- script.js
+++ script.js
@@ -172,6 +172,23 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username == currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  labelWelcome.textContent = 'Log in to get started';
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 // console.log(accounts);
 
 /////////////////////////////////////////////////
